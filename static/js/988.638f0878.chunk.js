841512809a48697c75ea1e198f2f77f4910af49d
TatianaSkl <116264242+TatianaSkl@users.noreply.github.com>
2024-02-28 18:48:30 +0000

    Deploying to gh-pages from @ TatianaSkl/burvin@645674a60cdf13a633d7070e4ab5423103108be7 🚀

--- static/js/988.638f0878.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkburvin=self.webpackChunkburvin||[]).push([[988],{7988:(t,n,r)=>{r.d(n,{Anj:()=>a});var e=r(1804);function a(t){return(0,e.YH)({tag:"svg",attr:{viewBox:"0 0 24 24",strokeWidth:"2",stroke:"currentColor",fill:"none",strokeLinecap:"round",strokeLinejoin:"round"},child:[{tag:"path",attr:{stroke:"none",d:"M0 0h24v24H0z",fill:"none"}},{tag:"path",attr:{d:"M19.933 13.041a8 8 0 1 1 -9.925 -8.788c3.899 -1 7.935 1.007 9.425 4.747"}},{tag:"path",attr:{d:"M20 4v5h-5"}}]})(t)}}}]);
-//# sourceMappingURL=988.638f0878.chunk.js.map
