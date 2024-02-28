cad5eb969a9d0c449c57af765b04ccf3c683894c
TatianaSkl <sklyarchyk@gmail.com>
2024-02-28 20:41:59 +0200

    fix

--- src/components/ModalVideo/ModalVideo.styled.jsx
+++ src/components/ModalVideo/ModalVideo.styled.jsx
@@ -43,7 +43,7 @@ export const ButtonClose = styled.button`
 export const Video = styled.iframe`
   width: auto;
   max-width: 100%;
-  height: 100vh;
+  height: 90vh;
   padding: 40px 0 40px;
   margin: 0 auto;
   @media (min-width: 768px) {
