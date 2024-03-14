1bcf962b8fb491967eeaaef8802571713e2b0715
TatianaSkl <sklyarchyk@gmail.com>
2024-03-14 17:35:29 +0200

    fix

--- src/components/UserNav/UserNav.styled.jsx
+++ src/components/UserNav/UserNav.styled.jsx
@@ -5,7 +5,10 @@ export const NavUser = styled.nav`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  height: calc(100vh - 110px);
+  height: calc(100vh - 150px);
+  @media (min-width: 768px) {
+    height: calc(100vh - 110px);
+  }
 `;
 
 export const Wrap = styled.div`
